b6c243fdb8b259ed486b1715130220f5a3585e3a
thaparojanthapa1-eng <thaparojanthapa1@gmail.com>
2026-05-27 19:52:58 +0545

    configure Flask app with SQLAlchemy

--- website/__init__.py
+++ website/__init__.py
@@ -1,6 +1,13 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+Db=SQLAlchemy()
+DB_NAME="database.db"
 
 def create_app():
     app=Flask(__name__)
+    app.config['SECRET_KEY']="Hello World"
+    app.config["SQLALCHEMY_DATABASE_URI"]=f"sqlite:///{DB_NAME}"
+    Db.init_app(app)
 
     return app
